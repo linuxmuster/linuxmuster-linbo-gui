896cedcea4056364f23b4cea2e81224ad00ab533
Dorian Zedler <dorian@itsblue.dev>
2020-12-03 15:21:43 +0100

    - reordered some stuff - new build system (yet to be tested)

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+## Linbo GUI global build srcipt
+## Copyright (C) 2020  Dorian Zedler <dorian@itsblue.de>
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Affero General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+##
+## You should have received a copy of the GNU Affero General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+## This script will build the linbo GUI for 32 and 64 bit.
+
+cd build
+
+# build for 64-Bit
+mkdir buildGUI64
+
+# create the buildfile
+cp build_Qt_and_Gui_generic buildGUI64/build
+cd buildGUI64
+
+sed -i '/## 32: /d' build
+sed -i 's/## 64: //g' build
+
+./build
+
+cd ..
+
+# build for 32-Bit
+mkdir buildGUI32
+
+# create the buildfile
+cp build_Qt_and_Gui_generic buildGUI32/build
+cd buildGUI32
+
+sed -i '/## 64: /d' build
+sed -i 's/## 32: //g' build
+
+./build
+
+cd ..
