ad1d13f0e997ba7620538c7952cd07405d574a27
Thomas Schmitt <thomas@linuxmuster.net>
2020-12-25 15:32:37 +0100

    excluded archives* from package build.

--- debian/mkdeb.sh
+++ debian/mkdeb.sh
@@ -3,7 +3,7 @@
 # builds debian package
 #
 # thomas@linuxmuster.net
-# 20201208
+# 20201225
 #
 # to invoke in the buildroot: debian/mkdeb.sh
 
@@ -12,6 +12,7 @@
 dpkg-buildpackage -rfakeroot -tc -sa --no-sign \
   -i.git \
   -I.git \
+  -Iarchives* \
   -Ibuild/*.tar.xz \
   -Ibuild/buildGUI* \
   -Ibuild/linbofs*
