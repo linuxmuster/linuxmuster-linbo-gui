ab02072364ea7a2873a02618ebcfe2430b0a11d8
GitHub Action <action@github.com>
2021-01-01 13:27:22 +0000

    New Translations from crowdin (by GitHub Action)

---
+++ resources/translations/linbo-gui/de-DE.ts
@@ -0,0 +1,256 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en" language="de">
+  <context>
+    <name>LinboClientInfo</name>
+    <message id="client_info_mac">
+      <source>Mac</source>
+      <oldsource>Mac:</oldsource>
+      <translation>Mac</translation>
+    </message>
+    <message id="client_info_hdd">
+      <source>HDD</source>
+      <oldsource>HDD:</oldsource>
+      <translation>HDD</translation>
+    </message>
+    <message id="client_info_cache">
+      <source>Cache</source>
+      <oldsource>Cache:</oldsource>
+      <translation>Cache</translation>
+    </message>
+    <message id="client_info_cpu">
+      <source>CPU</source>
+      <oldsource>CPU:</oldsource>
+      <translation>CPU</translation>
+    </message>
+    <message id="client_info_ram">
+      <source>RAM</source>
+      <oldsource>RAM:</oldsource>
+      <translation>RAM</translation>
+    </message>
+  </context>
+  <context>
+    <name>LinboImageCreationDialog</name>
+    <message id="dialog_createImage_title">
+      <source>Create image</source>
+      <translation>Image erstellen</translation>
+    </message>
+    <message id="dialog_createImage_action_title">
+      <source>The action to perfom:</source>
+      <translation>Auszuführende Aktion:</translation>
+    </message>
+    <message id="dialog_createImage_action_current">
+      <source>replace current image</source>
+      <translation>aktuelles Image ersetzen</translation>
+    </message>
+    <message id="dialog_createImage_action_new">
+      <source>create a new image</source>
+      <translation>neues Image erstellen</translation>
+    </message>
+    <message id="dialog_createImage_imageName">
+      <source>Image name:</source>
+      <translation>Image name:</translation>
+    </message>
+    <message id="dialog_createImage_imageDescription">
+      <source>Image description:</source>
+      <translation>Image Beschreibung:</translation>
+    </message>
+    <message id="dialog_createImage_postActionQuestion">
+      <source>What to do after the process has finished?</source>
+      <oldsource>What to do after the processes has finished?</oldsource>
+      <translation>Was soll nach dem Ende des Prozesses passieren?</translation>
+    </message>
+    <message id="dialog_createImage_postaction_nothing">
+      <source>nothing</source>
+      <translation>nichts</translation>
+    </message>
+    <message id="dialog_createImage_postaction_shutdown">
+      <source>shutdown</source>
+      <translation>herunterfahren</translation>
+    </message>
+    <message id="dialog_createImage_postaction_reboot">
+      <source>reboot</source>
+      <translation>neu starten</translation>
+    </message>
+    <message id="dialog_createImage_postaction_logout">
+      <source>logout</source>
+      <translation>abmelden</translation>
+    </message>
+    <message id="dialog_createImage_button_create">
+      <source>create</source>
+      <translation>erstellen</translation>
+    </message>
+    <message id="dialog_createImage_button_createAndUpload">
+      <source>create + upload</source>
+      <translation>erstellen + hochladen</translation>
+    </message>
+    <message id="cancel">
+      <source>cancel</source>
+      <translation>abbrechen</translation>
+    </message>
+  </context>
+  <context>
+    <name>LinboLoginDialog</name>
+    <message id="dialog_login_title">
+      <source>Please enter password:</source>
+      <translation>Bitte Passwort eingeben:</translation>
+    </message>
+    <message id="dialog_login_title_wrong">
+      <source>Wrong password!</source>
+      <translation>Falsches Passwort!</translation>
+    </message>
+  </context>
+  <context>
+    <name>LinboMainActions</name>
+    <message id="main_noBaseImage">
+      <source>No baseimage defined</source>
+      <translation>Kein Basisimage definiert</translation>
+    </message>
+    <message id="dialog_partition_title">
+      <source>Partition drive</source>
+      <translation>Festplatte partitionieren</translation>
+    </message>
+    <message id="dialog_partition_question">
+      <source>Are you sure? This will delete all data on your drive!</source>
+      <translation>Sind Sie sicher? Dadurch werden alle Daten auf Ihrem Laufwerk gelöscht!</translation>
+    </message>
+    <message id="main_root_button_createImage">
+      <source>Create image</source>
+      <translation>Image erstellen</translation>
+    </message>
+    <message id="main_root_button_uploadImage">
+      <source>Upload image</source>
+      <translation>Image hochladen</translation>
+    </message>
+    <message id="main_root_button_openTerminal">
+      <source>Open terminal</source>
+      <translation>Terminal öffnen</translation>
+    </message>
+    <message id="main_root_button_updateCache">
+      <source>Update cache</source>
+      <translation>Cache aktualisieren</translation>
+    </message>
+    <message id="main_root_button_partitionDrive">
+      <source>Partition drive</source>
+      <translation>Festplatte partitionieren</translation>
+    </message>
+    <message id="main_root_button_register">
+      <source>Register</source>
+      <translation>Registrieren</translation>
+    </message>
+    <message id="main_message_processCrashed">
+      <source>The process %1 crashed:</source>
+      <oldsource>The process &quot;%1&quot; crashed:</oldsource>
+      <translation>Der Prozess %1 ist abgestürzt:</translation>
+    </message>
+    <message>
+      <source>No logs before this crash</source>
+      <translation>Keine Meldungen vor diesem Absturz</translation>
+    </message>
+    <message id="main_message_lastLogs">
+      <source>The last logs before the crash were:</source>
+      <translation>Die letzten Meldungen vor dem Absturz waren:</translation>
+    </message>
+    <message id="main_message_lastErrors">
+      <source>The last errors before the crash were:</source>
+      <translation>Die letzten Fehler vor dem Absturz waren:</translation>
+    </message>
+    <message id="main_message_askForHelp">
+      <source>Please ask your system administrator for help.</source>
+      <translation>Bitten sie ihren Systemadministrator um Hilfe.</translation>
+    </message>
+    <message id="main_message_processFinishedSuccessfully">
+      <source>The process %1 finished successfully.</source>
+      <oldsource>The process &quot;%s&quot; finished successfully.</oldsource>
+      <translation>Der Prozess %1 wurde erfolgreich abgeschlossen.</translation>
+    </message>
+  </context>
+  <context>
+    <name>LinboOsSelectionRow</name>
+    <message id="osSelection_noOperatingSystems">
+      <source>No Operating system configured in start.conf</source>
+      <translation>Kein Betriebssystem in der start.conf konfiguriert</translation>
+    </message>
+  </context>
+  <context>
+    <name>LinboRegisterDialog</name>
+    <message id="dialog_register_title">
+      <source>Register client</source>
+      <translation>Client registrieren</translation>
+    </message>
+    <message id="room">
+      <source>Room</source>
+      <translation>Raum</translation>
+    </message>
+    <message id="hostname">
+      <source>Hostname</source>
+      <translation>Hostname</translation>
+    </message>
+    <message id="ip">
+      <source>IP-Address</source>
+      <translation>IP-Adresse</translation>
+    </message>
+    <message id="group">
+      <source>Host group</source>
+      <translation>Host-Gruppe</translation>
+    </message>
+    <message id="dialog_register_clientRole">
+      <source>Client role</source>
+      <translation>Client-Rolle</translation>
+    </message>
+    <message id="dialog_register_clientRole_classroomStudent">
+      <source>Classroom student computer</source>
+      <translation>Schüler-PC im Klassenzimmer</translation>
+    </message>
+    <message id="dialog_register_clientRole_classroomTeacher">
+      <source>Classroom teacher computer</source>
+      <translation>Lehrer-PC im Klassenzimmer</translation>
+    </message>
+    <message id="dialog_register_clientRole_facultyTeacher">
+      <source>Faculty teacher computer</source>
+      <translation>Fachbereich-Lehrer-PC</translation>
+    </message>
+    <message id="dialog_register_clientRole_staffComputer">
+      <source>Staff computer</source>
+      <translation>Personalcomputer</translation>
+    </message>
+    <message id="dialog_register_button_resgister">
+      <source>register</source>
+      <translation>registrieren</translation>
+    </message>
+  </context>
+  <context>
+    <name>LinboTerminalDialog</name>
+    <message id="dialog_terminal_title">
+      <source>Terminal</source>
+      <translation>Terminal</translation>
+    </message>
+  </context>
+  <context>
+    <name>LinboUpdateCacheDialog</name>
+    <message id="dialog_updateCache_title">
+      <source>Update cache</source>
+      <translation>Cache aktualisieren</translation>
+    </message>
+    <message id="dialog_updateCache_updateType_rsync">
+      <source>Update using rsync</source>
+      <translation>Mit Rsync aktualisieren</translation>
+    </message>
+    <message id="dialog_updateCache_updateType_multicast">
+      <source>Update using multicast</source>
+      <translation>Mit Multicast aktualisieren</translation>
+    </message>
+    <message id="dialog_updateCache_updateType_torrent">
+      <source>Update using torrent</source>
+      <translation>Mit Torrent aktualisieren</translation>
+    </message>
+    <message id="dialog_updateCache_formatPartition">
+      <source>Format cache partition</source>
+      <translation>Cache-Partition formatieren</translation>
+    </message>
+    <message id="dialog_updateCache_button_update">
+      <source>update</source>
+      <translation>aktualisieren</translation>
+    </message>
+  </context>
+</TS>
